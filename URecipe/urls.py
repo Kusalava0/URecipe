5dfbbd96931ddcd79e3ec858674858b07a5b767b
Kusalava <dkb03122001@gmail.com>
2022-10-13 03:03:16 +0530

    images not showing fix

--- URecipe/urls.py
+++ URecipe/urls.py
@@ -15,3 +15,5 @@ urlpatterns = [
     path("register/", views.register),
 
 ] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
