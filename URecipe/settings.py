00ea5d119fa6d4a61206d3f6566924a60f399b7d
Kusalava <dkb03122001@gmail.com>
2022-10-13 02:26:09 +0530

    static dir changed

--- URecipe/settings.py
+++ URecipe/settings.py
@@ -102,7 +102,9 @@ STATIC_URL = "static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 MEDIA_URL = "/media/"
 
